ce649b8fc8362cbdce7a84001673c3d24e36eac1
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-15 16:17:40 +0100

    feat: Add profiles following TOP lesson

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
+import Spinach from "./components/Spinach";
+import Popeye from "./components/Popeye";
 
 const router = createBrowserRouter([
   {
@@ -12,11 +14,15 @@ const router = createBrowserRouter([
   {
     path: "profile",
     element: <Profile />,
+    children: [
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
