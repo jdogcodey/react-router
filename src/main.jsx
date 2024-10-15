ca5cafbc8b119d004785426e54474c6562e7836d
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-15 16:13:29 +0100

    fix: Change a to Link tag. Following TOP lesson

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Profile from "./Profile";
+import Profile from "./components/Profile";
 
 const router = createBrowserRouter([
   {
