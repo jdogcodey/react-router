5d9dcf8f8436f4b51f707dfc3dccaa0c23a2463b
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-15 16:26:36 +0100

    fix: Add ErrorPage following TOP lesson

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,17 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
-import Spinach from "./components/Spinach";
-import Popeye from "./components/Popeye";
-import DefaultProfile from "./components/DefaultProfile";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />
   },
   {
-    path: "profile",
+    path: "profile/:name",
     element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
   },
 ]);
 
