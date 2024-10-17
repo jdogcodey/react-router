8c3776c50774d9592990bf94059dcec925c8143c
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-17 15:21:14 +0100

    Finishing up tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Profile from "./components/Profile";
-import ErrorPage from "./components/ErrorPage";
+import routes from "./components/routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "profile/:name",
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
